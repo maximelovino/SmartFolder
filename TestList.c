43443cda624d46549fe797f397aafbbbc854b0bc
Osoldier <thomas.ibanez33@gmail.com>
2016-11-25 09:41:42 +0100

    added list structure, thanks @maximelovino for at least half of it

---
+++ TestList.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "List.h"
+
+int main() {
+    List* l1 = initList();
+    for (size_t i = 0; i < 10; i++) {
+        char ch = 'a';
+        char* string = malloc(2);
+        string[0] = ch;
+        string[1] = (char) (ch+(char)i);
+        insert(l1, string);
+    }
+
+    List* l2 = initList();
+    for (size_t i = 5; i < 12; i++) {
+        char ch = 'a';
+        char* string = malloc(2);
+        string[0] = ch;
+        string[1] = (char) (ch+(char)i);
+        insert(l2, string);
+    }
+    printf("LOL l1 is happening\n");
+    for (size_t i = 0; i < l1->size; i++) {
+        printf("%s\n", get(l1, i));
+    }
+    printf("LOL l2 is happening\n");
+    for (size_t i = 0; i < l2->size; i++) {
+        printf("%s\n", get(l2, i));
+    }
+
+    printf("intersection\n");
+    List* intersectList = listIntersect(l1, l2);
+    for (size_t i = 0; i < intersectList->size; i++) {
+        printf("%s\n", get(intersectList,i));
+    }
+    printf("union\n");
+    List* unionList = listUnion(l1, l2);
+    for (size_t i = 0; i < unionList->size; i++) {
+        printf("%s\n", get(unionList,i));
+    }
+
+    return 0;
+}
