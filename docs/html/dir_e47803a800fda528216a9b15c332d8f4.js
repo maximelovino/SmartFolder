b37eab196271cbb3a63ccd7750042884f9aeab1d
Maxime Lovino <maximelovino@gmail.com>
2017-01-27 17:30:36 +0100

    Changed doc to be at root of /docs/, thus enabling github pages

--- docs/html/dir_e47803a800fda528216a9b15c332d8f4.js
+++
@@ -1,4 +0,0 @@
-var dir_e47803a800fda528216a9b15c332d8f4 =
-[
-    [ "src", "dir_a6d2e3f8d89e356bae799b4197e613c9.html", "dir_a6d2e3f8d89e356bae799b4197e613c9" ]
-];
--- docs/html/files.js
+++
@@ -1,4 +0,0 @@
-var files =
-[
-    [ "SmartFolder", "dir_e47803a800fda528216a9b15c332d8f4.html", "dir_e47803a800fda528216a9b15c332d8f4" ]
-];
