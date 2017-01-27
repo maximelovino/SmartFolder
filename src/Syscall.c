ed61ede5cfae5879a1942f510cb4e97fc118fae1
Osoldier <thomas.ibanez33@gmail.com>
2017-01-27 09:34:43 +0100

    Changed syscall

--- src/Syscall.c
+++ src/Syscall.c
@@ -9,81 +9,124 @@
 
 #include "Syscall.h"
 
-int sfork() {
-	return fork();
+inline int sfork() {
+	int v = fork();
+	if(v == -1) {
+		logMessage(3, "fork failed !");
+		_exit(1);
+	}
+	return v;
 }
 
-int ssymlink(char* target, char* linkPath) {
-	return symlink(target, linkPath);
+inline void ssymlink(char* target, char* linkPath) {
+	if(symlink(target, linkPath) == -1) {
+		logMessage(3, "symlink failed ! %s %s", target, linkPath);
+		_exit(1);
+	}
 }
 
-char* sbasename(char* fullPath) {
-	return basename(fullPath);
+inline char* sbasename(char* fullPath) {
+	char* name = basename(fullPath);
+	if(name == NULL) {
+		logMessage(3, "basename failed ! %s", fullPath);
+		_exit(1);
+	}
+	return name;
 }
 
-int smkdir(char* path) {
-	return mkdir(path, 0775);
+inline void smkdir(char* path) {
+	int r = mkdir(path, 0775);
+	if(r != 0) {
+		logMessage(3, "mkdir failed ! %s", path);
+		_exit(1);
+	}
 }
 
-FILE* stouch(char* file) {
-	return fopen(file, "w+");
+inline FILE* stouch(char* file) {
+	FILE* fp = fopen(file, "w+");
+	if(fp == NULL) {
+		logMessage(3, "touch failed ! %s", file);
+		_exit(1);
+	}
+	return fp;
 }
 
-int saccess(char* path) {
+inline int saccess(char* path) {
 	return access(path, F_OK) != -1;
 }
 
-int sunlink(char* file) {
+inline void sunlink(char* file) {
 	if (saccess(file)) {
-		return unlink(file);
+		int i = unlink(file);
+		if(i == 0) return;
 	}
-	return -1;
+	logMessage(3, "unlink failed ! %s", file);
+	_exit(1);
 }
 
-int srmdir(char* path) {
-	return rmdir(path);
+inline void srmdir(char* path) {
+	int i = rmdir(path);
+	if(i != 0) {
+		logMessage(3, "rmdir failed ! %s", path);
+		_exit(1);
+	}
 }
 
-int sgetpwuid(char* userName) {
+inline int sgetpwuid(char* userName) {
 	return getpwnam(userName)->pw_uid;
 }
 
-int sgetgrgid(char* groupName) {
+inline int sgetgrgid(char* groupName) {
 	return getgrnam(groupName)->gr_gid;
 }
 
-int slstat(char* name, struct stat* statbuf) {
-	return lstat(name, statbuf);
+inline int slstat(char* name, struct stat* statbuf) {
+	int i = lstat(name, statbuf);
+	if(i != 0) {
+		logMessage(3, "lstat failed ! %s", name);
+		_exit(1);
+	}
 }
 
-DIR* sopendir(char* name) {
-	return opendir(name);
+inline DIR* sopendir(char* name) {
+	DIR* dp = opendir(name);
+	if(dp == NULL) {
+		logMessage(3, "opendir failed ! %s", name);
+		_exit(1);
+	}
+	return dp;
 }
 
-int schdir(char* name) {
-	return chdir(name);
+inline void schdir(char* name) {
+	if(chdir(name) != 0) {
+		logMessage(3, "chdir failed ! %s", name);
+		_exit(1);
+	}
 }
 
-int sclosedir(DIR* dp) {
-	return closedir(dp);
+inline void sclosedir(DIR* dp) {
+	if(closedir(dp) != 0) {
+		logMessage(3, "closedir failed !");
+		_exit(1);
+	}
 }
 
-struct dirent* sreaddir(DIR* dir) {
+inline struct dirent* sreaddir(DIR* dir) {
 	return readdir(dir);
 }
 
-int sS_ISLNK(mode_t m) {
+inline int sS_ISLNK(mode_t m) {
 	return S_ISLNK(m);
 }
 
-int sS_ISDIR(mode_t m) {
+inline int sS_ISDIR(mode_t m) {
 	return S_ISDIR(m);
 }
 
-int sS_ISREG(mode_t m) {
+inline int sS_ISREG(mode_t m) {
 	return S_ISREG(m);
 }
 
-char* srealpath(char* p, char* buf) {
+inline char* srealpath(char* p, char* buf) {
 	return realpath(p, buf);
 }
--- src/Syscall.h
+++ src/Syscall.h
@@ -19,43 +19,134 @@
 #include <stdio.h>
 #include <pwd.h>
 #include <grp.h>
+#include "Logger.h"
 
+/**
+ * fork wrapper
+ * @return pid of the child or 0 if you are the child
+ */
 int sfork();
 
-int ssymlink(char* target, char* linkPath);
+/**
+ * symlink wrapper
+ * @param  target   real file
+ * @param  linkPath link path
+ */
+void ssymlink(char* target, char* linkPath);
 
+/**
+ * basename wrapper
+ * @param  fullPath full path of the file
+ * @return          basename of the file
+ */
 char* sbasename(char* fullPath);
 
-int smkdir(char* path);
+/**
+ * mkdir wrapper
+ * @param  path path to create
+ */
+void smkdir(char* path);
 
+/**
+ * touch wrapper
+ * @param  file file to create
+ * @return      FILE* to the file created
+ */
 FILE* stouch(char* file);
 
+/**
+ * access wrapper
+ * @param  path file to check
+ * @return      1 if the file exists, 0 otherwise
+ */
 int saccess(char* path);
 
-int sunlink(char* file);
+/**
+ * unlink wrapper
+ * @param  file file to unlink
+ */
+void sunlink(char* file);
 
-int srmdir(char* path);
+/**
+ * rmdir wrapper
+ * @param  path directory to remove
+ */
+void srmdir(char* path);
 
+/**
+ * getpwnam->pw_uid wrapper
+ * @param  userName the username to get uid from
+ * @return          uid linked to the username
+ */
 int sgetpwuid(char* userName);
 
+/**
+ * getgrnam->gr_gid wrapper
+ * @param  groupName groupname to get gid from
+ * @return           gid linked to this groupname
+ */
 int sgetgrgid(char* groupName);
 
-int slstat(char* name, struct stat* statbuf);
+/**
+ * lstat wrapper
+ * @param  name    file to check
+ * @param  statbuf buffer to write info to
+ */
+void slstat(char* name, struct stat* statbuf);
 
+/**
+ * opendir wrapper
+ * @param  name directory to open
+ * @return      DIR* to the directory
+ */
 DIR* sopendir(char* name);
 
-int schdir(char* name);
+/**
+ * chdir wrapper
+ * @param  name directory to go to
+ */
+void schdir(char* name);
 
-int sclosedir(DIR* dp);
+/**
+ * closedir wrapper
+ * @param  dp directory to close
+ */
+void sclosedir(DIR* dp);
 
+/**
+ * readdir wrapper
+ * @param  dir directory to read
+ * @return     dirent entries
+ */
 struct dirent* sreaddir(DIR* dir);
 
+/**
+ * S_ISLNK wrapper
+ * @param  m file to check
+ * @return   1 if the file is a link, 0 otherwise
+ */
 int sS_ISLNK(mode_t m);
 
+/**
+ * S_ISDIR wrapper
+ * @param  m file to check
+ * @return   1 if the file is a directory, 0 otherwise
+ */
 int sS_ISDIR(mode_t m);
 
+/**
+ * S_ISREG wrapper
+ * @param  m file to check
+ * @return   1 if the file is a regular file, 0 otherwise
+ */
 int sS_ISREG(mode_t m);
 
+/**
+ * realpath wrapper
+ * @param  p   file to get realpath from
+ * @param  buf buffer to write the realpath to (optional)
+ * @return     if the buffer is NULL, return the realpath
+ */
 char* srealpath(char* p, char* buf);
 
 #endif /* end of include guard: _SYSCALL_H_ */
